1be28d3220051cb9b7e59b2c71cdd7f030320194
Shardul-008 <shardulkukreti008@gmail.com>
2021-01-29 11:29:51 +0530

    Files Added

---
+++ MenuItemListing/Controllers/MenuItemController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//.
+//.
+using MenuItemListing.Model;
+using Microsoft.AspNetCore.Authorization;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MenuItemListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MenuItemController : ControllerBase
+    {
+
+
+        public List<MenuItem> menuItems = new List<MenuItem>
+            {
+                new MenuItem(){ Id = 101, Name = "Burger", Active=true, DateOfLaunch = DateTime.Now.AddDays(1), FreeDelivery = false, Price = 45},
+                new MenuItem(){ Id = 102, Name = "Pizza", Active=true, DateOfLaunch = DateTime.Now, FreeDelivery = true, Price = 100}
+            };
+        // GET: api/<MenuItemController>
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            return menuItems;
+        }
+
+        // GET api/<MenuItemController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return menuItems.FirstOrDefault(menuItem => menuItem.Id == id).Name;
+        }
+    }
+}
---
+++ MenuItemListing/Model/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing.Model
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public bool Active { get; set; }
+
+        [Display(Name = "Date of Launch")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfLaunch { get; set; }
+        [Display(Name = "Free Delivery")]
+        public bool FreeDelivery { get; set; }
+    }
+}
